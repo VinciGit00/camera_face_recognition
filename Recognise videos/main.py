a08d19084d37decb193bc476897f4e33eb5b5aef
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-05-04 12:28:40 +0300

    add configurations modification

--- Recognise videos/main.py
+++ Recognise videos/main.py
@@ -3,7 +3,7 @@ import cv2
 cap = cv2.VideoCapture("highway.mp4")
 
 #Object detection from stable camera
-object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=5)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
 while True:
     #Each loop take 1 frame
@@ -16,6 +16,7 @@ while True:
 
     #Object detection
     mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
 
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -25,8 +26,10 @@ while True:
         area = cv2.contourArea(cnt)
         if(area>100):
             #Add just the big elements
-            cv2.drawContours(roi, [cnt], -1, (0,255,0), 2)
-
+            #cv2.drawContours(roi, [cnt], -1, (0,255,0), 2)
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(roi, (x,y), (x+w, y+h), (0, 255, 0), 3)
+             
     cv2.imshow("Roi", roi)
     #Show the frame with just the white and the black
     cv2.imshow("Mask", mask)
